876151b712ce2d64cc7ae639fd484439865a5807
Alexandre Magno <alexanmtz@gmail.com>
2019-09-04 23:04:40 +0200

    adding tests that return action functions

--- src/genie-actions.js
+++ src/genie-actions.js
@@ -1,16 +1,49 @@
 const genieActionNames = (entity, types, states) => {
-    const upperEntity = entity.toUpperCase();
-    const actionTypes = types || ['create', 'update', 'delete', 'list', 'fetch'];
-    const stateTypes = states || ['REQUESTED', 'SUCCESS', 'ERROR']
-    let actions = {};
-    actionTypes.forEach((t) => {
-        const typeUpper = t.toUpperCase();
-        actions[t] = stateTypes.map(s => {
-            const stateNames = s.toUpperCase();
-            return `${typeUpper}_${upperEntity}_${stateNames}`, `${typeUpper}_${upperEntity}_${stateNames}`, `${typeUpper}_${upperEntity}_${stateNames}`;
-        });
+  const upperEntity = entity.toUpperCase();
+  const actionTypes = types || ['create', 'update', 'delete', 'list', 'fetch'];
+  const stateTypes = states || ['REQUESTED', 'SUCCESS', 'ERROR']
+  let actions = {};
+  actionTypes.forEach((t) => {
+    const typeUpper = t.toUpperCase();
+    actions[t] = stateTypes.map(s => {
+      const stateNames = s.toUpperCase();
+      return `${typeUpper}_${upperEntity}_${stateNames}`, `${typeUpper}_${upperEntity}_${stateNames}`, `${typeUpper}_${upperEntity}_${stateNames}`;
     });
-    return actions;
+  });
+  return actions;
 }
 
-export { genieActionsNames };
+const genieActions = (entity) => {
+  const actionNames = genieActionNames(entity)
+  let actionCreators = []
+
+  Object.keys(actionNames).forEach(key => {
+    actionCreators.push(actionNames[key].map((item, index) => {
+        let action;
+        switch(index) {
+          case 0:
+            action = new Function(`return { type: '${item}', completed: false }`)
+            break;
+          case 1:
+            action = new Function(`return { type: '${item}', completed: true, error: false }`)
+            if(key === 'list') {
+              action = new Function('data', `return { type: '${item}', completed: true, error: false, data }`)
+            }
+            if(key === 'fetch') {
+              action = new Function('data', `return { type: '${item}', completed: true, error: false, data }`)
+            }
+            break;
+          case 2:
+            action = new Function('error', `return { type: '${item}', completed: true, error }`)
+            break;
+          default:
+            action = () => ({ type: 'UNKNOWN' })
+            break;
+        }
+        return action
+    }))
+  })
+  return Array.prototype.concat.apply([], actionCreators)
+}
+
+export { genieActionNames, genieActions };
--- tests/actions.test.js
+++ tests/actions.test.js
@@ -1,23 +1,64 @@
-import { genieActionNames } from '../src/genie-actions'
+import { genieActionNames, genieActions } from '../src/genie-actions'
 
-describe('basic actions', () => {
-  test('Get default actions from genie', () => {
-    expect(genieActionNames('user')).toEqual({
-      'create': ['CREATE_USER_REQUESTED', 'CREATE_USER_SUCCESS', 'CREATE_USER_ERROR'],
-      'update': ['UPDATE_USER_REQUESTED', 'UPDATE_USER_SUCCESS', 'UPDATE_USER_ERROR'],
-      'delete': ['DELETE_USER_REQUESTED', 'DELETE_USER_SUCCESS', 'DELETE_USER_ERROR'],
-      'list': ['LIST_USER_REQUESTED', 'LIST_USER_SUCCESS', 'LIST_USER_ERROR'],
-      'fetch': ['FETCH_USER_REQUESTED', 'FETCH_USER_SUCCESS', 'FETCH_USER_ERROR'],
+describe('Genie', () => {
+  describe('Action names', () => {
+    test('Get default actions from genie', () => {
+      expect(genieActionNames('user')).toEqual({
+        'create': ['CREATE_USER_REQUESTED', 'CREATE_USER_SUCCESS', 'CREATE_USER_ERROR'],
+        'update': ['UPDATE_USER_REQUESTED', 'UPDATE_USER_SUCCESS', 'UPDATE_USER_ERROR'],
+        'delete': ['DELETE_USER_REQUESTED', 'DELETE_USER_SUCCESS', 'DELETE_USER_ERROR'],
+        'list': ['LIST_USER_REQUESTED', 'LIST_USER_SUCCESS', 'LIST_USER_ERROR'],
+        'fetch': ['FETCH_USER_REQUESTED', 'FETCH_USER_SUCCESS', 'FETCH_USER_ERROR'],
+      });
+    });
+    test('Get custom actions from genie', () => {
+      expect(genieActionNames('user', ['login'])).toEqual({
+        'login': ['LOGIN_USER_REQUESTED', 'LOGIN_USER_SUCCESS', 'LOGIN_USER_ERROR']
+      });
+    });
+    test('Get custom states from genie', () => {
+      expect(genieActionNames('user', ['login'], ['started', 'ready', 'error'])).toEqual({
+        'login': ['LOGIN_USER_STARTED', 'LOGIN_USER_READY', 'LOGIN_USER_ERROR']
+      });
     });
   });
-  test('Get custom actions from genie', () => {
-    expect(genieActionNames('user', ['login'])).toEqual({
-      'login': ['LOGIN_USER_REQUESTED', 'LOGIN_USER_SUCCESS', 'LOGIN_USER_ERROR']
-    });
-  });
-  test('Get custom states from genie', () => {
-    expect(genieActionNames('user', ['login'], ['started', 'ready', 'error'])).toEqual({
-      'login': ['LOGIN_USER_STARTED', 'LOGIN_USER_READY', 'LOGIN_USER_ERROR']
+  describe('Action generators', () => {
+    test('Get actions from genie', () => {
+      expect(genieActions('user', 'http://example.com/user/').map((r, index) => {
+        if(index === 2) {
+          return r({})
+        } else if(index === 5) {
+          return r({})
+        } else if(index === 8) {
+          return r({})
+        } else if(index === 10) {
+          return r([])
+        } else if(index === 11) {
+          return r({})
+        } else if(index === 13) {
+          return r({})
+        } else if(index === 14) {
+          return r({})
+        } else {
+          return r()
+        }
+      })).toEqual([
+        { type: 'CREATE_USER_REQUESTED', completed: false },
+        { type: 'CREATE_USER_SUCCESS', completed: true, error: false },
+        { type: 'CREATE_USER_ERROR', completed: true, error: {} },
+        { type: 'UPDATE_USER_REQUESTED', completed: false },
+        { type: 'UPDATE_USER_SUCCESS', completed: true, error: false },
+        { type: 'UPDATE_USER_ERROR', completed: true, error: {} },
+        { type: 'DELETE_USER_REQUESTED', completed: false },
+        { type: 'DELETE_USER_SUCCESS', completed: true, error: false },
+        { type: 'DELETE_USER_ERROR', completed: true, error: {} },
+        { type: 'LIST_USER_REQUESTED', completed: false },
+        { type: 'LIST_USER_SUCCESS', completed: true, error: false, data: [] },
+        { type: 'LIST_USER_ERROR', completed: true, error: {} },
+        { type: 'FETCH_USER_REQUESTED', completed: false },
+        { type: 'FETCH_USER_SUCCESS', completed: true, error: false, data: {} },
+        { type: 'FETCH_USER_ERROR', completed: true, error: {} }
+      ]);
     });
   });
 })
